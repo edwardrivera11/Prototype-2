31b3affff57ab5fcf696d4dcd2817f3d8c85fc11
edwardrivera11 <98423321+edwardrivera11@users.noreply.github.com>
2022-10-28 12:34:14 -0400

    2.1

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -14,9 +14,16 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float speed = 10.0f;
-
+    public float xRange = 10;
+    public GameObject projectilePrefab;
     void Update()
     {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {// Launch a projectile from the player
+      Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+      }
+
+
        // keep the player in bounds
        horizontalInput =  Input.GetAxis("Horizontal"); 
        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
